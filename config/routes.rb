32905355d5b65524fa7b7ca3f3ed555b2f4a1752
Pedro Coelho <punnie@ethereal.io>
2019-08-31 16:02:51 +0100

    Set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :manifesto_items
   resources :manifesto_sections
   resources :manifestos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
 end
