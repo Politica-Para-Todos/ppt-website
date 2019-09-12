65734bfd577444d3ae2f60c24eaae0392a259363
Pedro Coelho <punnie@ethereal.io>
2019-09-13 00:01:02 +0100

    Tidy up code in migrations

--- db/migrate/20190902224345_add_comments_cache_to_manifesto_items.rb
+++ db/migrate/20190902224345_add_comments_cache_to_manifesto_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCommentsCacheToManifestoItems < ActiveRecord::Migration[6.0]
   def change
     add_column :manifesto_items, :comments_count, :integer
--- db/migrate/20190902225255_add_annotation_to_manifesto_items.rb
+++ db/migrate/20190902225255_add_annotation_to_manifesto_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAnnotationToManifestoItems < ActiveRecord::Migration[6.0]
   def change
     add_reference :manifesto_items, :annotation, null: true, foreign_key: true
