4d8db53ffcc10cd3ff23a94f6b0c189ee20a488c
Ines Rosete <inesrosete@gmail.com>
2019-09-24 00:16:15 +0100

    Fix bug

--- app/javascript/components/common/SocialIcon.js
+++ app/javascript/components/common/SocialIcon.js
@@ -1,4 +1,4 @@
-i/*
+/*
 Copyright 2019 Politica para Todos
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-mport React from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 
 const SocialIcon = (props) => {
