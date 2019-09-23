626bf1f917d847e2220e523b765bba54c03d1b66
Ines Rosete <inesrosete@gmail.com>
2019-09-23 23:22:06 +0100

    Generalize RoundAvatar and remove unused files

--- app/javascript/components/common/RoundAvatar.js
+++ app/javascript/components/common/RoundAvatar.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Avatar from 'antd/es/avatar';
 
 
-function RoundAvatar({ imageUrl, title, acronym }) {
+function RoundAvatar({ imageUrl, title, subtitle, link }) {
     return (
-        <a href={`party/${acronym}`}>
+        <a href={link}>
             <div className="avatar-list-item">
                 <Avatar size={120} src={imageUrl} icon="user" />
                 <h3 className="avatar-list-item__title">{title}</h3>
-                <h4 className="avatar-list-item__subtitle">{acronym}</h4>
+                <h4 className="avatar-list-item__subtitle">{subtitle}</h4>
             </div>
         </a>
     )
@@ -18,11 +18,11 @@ function RoundAvatar({ imageUrl, title, acronym }) {
 RoundAvatar.propTypes = {
     imageUrl: PropTypes.string,
     title: PropTypes.string.isRequired,
-    acronym: PropTypes.string,
+    subtitle: PropTypes.string,
 }
 
 RoundAvatar.defaultProps = {
     imageUrl: null,
-    acronym: null,
+    subtitle: null,
 };
 export default RoundAvatar;
