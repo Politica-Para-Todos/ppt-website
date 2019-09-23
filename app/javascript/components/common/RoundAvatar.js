a3f6b997d1c083ba612db01e67081f60d3cfb60b
João Gonçalves <jagoncalves@outlook.pt>
2019-09-23 22:07:06 +0100

    Change logo variable to imageUrl

--- app/javascript/components/common/RoundAvatar.js
+++ app/javascript/components/common/RoundAvatar.js
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 import Avatar from 'antd/es/avatar';
 
 
-function RoundAvatar({ logo, title, acronym }) {
+function RoundAvatar({ imageUrl, title, acronym }) {
     return (
-      <a href={`party/${acronym}`}>
-        <div className="avatar-list-item">
-            <Avatar size={120} src={logo} icon="user" />
-            <h3 className="avatar-list-item__title">{title}</h3>
-            <h4 className="avatar-list-item__subtitle">{acronym}</h4>
-        </div>
-      </a>
+        <a href={`party/${acronym}`}>
+            <div className="avatar-list-item">
+                <Avatar size={120} src={imageUrl} icon="user" />
+                <h3 className="avatar-list-item__title">{title}</h3>
+                <h4 className="avatar-list-item__subtitle">{acronym}</h4>
+            </div>
+        </a>
     )
 }
 
 RoundAvatar.propTypes = {
-    logo: PropTypes.string,
+    imageUrl: PropTypes.string,
     title: PropTypes.string.isRequired,
     acronym: PropTypes.string,
 }
 
 RoundAvatar.defaultProps = {
-    logo: null,
+    imageUrl: null,
     acronym: null,
 };
 export default RoundAvatar;
