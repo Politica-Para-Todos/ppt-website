9b6f4050ce53f6e988fa9ebe7074f28fd6cfad4c
Ines Rosete <inesrosete@gmail.com>
2019-09-26 10:14:15 +0100

    Remove seeds.rb content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ManifestoItem.destroy_all
-ManifestoSection.where.not(manifesto_section_id: nil).destroy_all
-ManifestoSection.destroy_all
-Manifesto.destroy_all
-Candidate.destroy_all
-Party.destroy_all
