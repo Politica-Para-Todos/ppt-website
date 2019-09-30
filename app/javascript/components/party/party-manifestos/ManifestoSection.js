a12749c9300e3098c36c5f7e25b36239ce78a049
Nuno Neves <nuno.neves@feedzai.com>
2019-09-30 14:28:34 +0100

    Add sidebar in manifesto page to share content

--- app/javascript/components/party/party-manifestos/ManifestoSection.js
+++ app/javascript/components/party/party-manifestos/ManifestoSection.js
@@ -1,4 +1,5 @@
 import React, { PureComponent, Fragment } from "react";
+import PropTypes from "prop-types";
 import { Tooltip } from "antd";
 import ReactHtmlParser from 'react-html-parser';
 import Popover from "react-text-selection-popover";
@@ -11,18 +12,40 @@ class ManifestoSection extends PureComponent {
         this.sectionContentRef = React.createRef();
     }
 
-    onClickTwitterShare(e) {
+    static propTypes = {
+        title: PropTypes.string,
+        section: PropTypes.object,
+        section_id: PropTypes.string,
+        party_acronym: PropTypes.string
+    }
+
+    onClickTwitterShare(e, id) {
         e.preventDefault();
-        const text = window.getSelection().toString();
+
+        const { section_id, party_acronym } = this.props;
+        const text = `https://www.politicaparatodos.pt/party/${party_acronym}/manifesto/${section_id}%23${id}`;
+        console.log(text);
         window.open(`https://twitter.com/intent/tweet?text=${text}`, "Twitter", "height=285,width=550,resizable=1");
         e.stopPropagation();
     }
 
-    renderSectionItem(item) {
+    renderTooltipContent() {
         return (
-            <Fragment key={item.id}>
+            <div onClick={(e) => this.onClickTwitterShare(e)}>
+                <img src={twitterImg} />
+            </div>
+        )
+    }
+    renderSectionItem(item) {
+        if (item.content.length === 1) {
+            return null;
+        }
+
+        return (
+            <div key={item.id} id={item.id} className="party-manifesto-item">
+                <div className="party-manifesto-item__bar" onClick={(e) => this.onClickTwitterShare(e, item.id)}></div>
                 {ReactHtmlParser(item.content)}
-            </Fragment>
+            </div>
         );
     }
 
@@ -54,18 +77,9 @@ class ManifestoSection extends PureComponent {
                 <h1 className="party-manifesto-body__title">{title}</h1>
                 {this.renderSectionTitle()}
                 <div
-                    ref={this.sectionContentRef}
                     className="party-manifesto-body__content">
                     {this.renderSectionContent()}
                 </div>
-                <Popover
-                    selectionRef={this.sectionContentRef}
-                    className="party-manifesto__share-popover">
-                    <div className="party-manifesto__share-popover-inner"
-                        onClick={(e) => this.onClickTwitterShare(e)}>
-                        <img src={twitterImg} />
-                    </div>
-                </Popover>
             </section>
         )
     }
