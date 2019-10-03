ed9c4514b7d253c89443108f42429f475829258e
João Romão <joao@getsocial.io>
2019-10-03 14:39:49 +0100

    changes filename for our banner and corrects its calls

--- app/javascript/components/about-us/index.js
+++ app/javascript/components/about-us/index.js
@@ -32,7 +32,7 @@ export default function AboutUs() {
                 pageDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
                 socialTitle="Quem somos"
                 socialDescription="A comunidade Política Para Todos nasceu no verão de 2019 com o objetivo de promover a participação ativa dos cidadãos nos processos eleitorais em Portugal. É composta por voluntários de várias partes do país e diferentes áreas profissionais."
-                socialImage="/images/share/banner-PtP.jpg"
+                socialImage="/images/share/banner-PPT.jpg"
             />
             <LayoutHeader />
             <Layout.Content className="about-us-section">
